b46e89092c43b30e82b7e416ca7d4b6a92bf5257
federicoachaneckar <federico.achaneckar@fds-esc.edu.ar>
2026-02-21 18:24:56 -0300

    Fix blank screen routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-react-app1/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
